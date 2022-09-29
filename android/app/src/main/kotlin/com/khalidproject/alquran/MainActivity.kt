fc66293726213edcca0f3f0ca5d77f33a3055a1f
khalid-701 <khalidramdhan97@gmail.com>
2022-09-29 16:19:30 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/khalidproject/alquran/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khalidproject.alquran
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+void main() async {
+  Uri url =  Uri.parse("https://api.quran.gading.dev/surah") ;
+ var res = await http.get(url);
+
+ List data =(json.decode(res.body) as Map<String, dynamic>)["data"];
+ print(data);
+}
