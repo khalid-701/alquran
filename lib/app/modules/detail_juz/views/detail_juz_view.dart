2ba48efb970980bac0608084eb13b3610396ac26
khalid-701 <khalidramdhan97@gmail.com>
2022-10-06 00:09:43 +0700

    surah in juz

--- lib/app/modules/detail_juz/views/detail_juz_view.dart
+++ lib/app/modules/detail_juz/views/detail_juz_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
-
 import '../../../data/models/juz/juz.dart' as juz;
 import '../../../data/models/surah/surah.dart';
 import '../controllers/detail_juz_controller.dart';
@@ -18,94 +17,119 @@ class DetailJuzView extends GetView<DetailJuzController> {
       print(element.name!.transliteration!.id);
     });
     return Scaffold(
-      appBar: AppBar(
-        title: Text('JUZ ${detailJuz.juz}', style: TextStyle(color: Get.isDarkMode ? Colors.white : Colors.black)),
-        iconTheme: IconThemeData(
-          color: Get.isDarkMode ? Colors.white:Colors.black, //change your color here
+        appBar: AppBar(
+          title: Text('JUZ ${detailJuz.juz}',
+              style: TextStyle(
+                  color: Get.isDarkMode ? Colors.white : Colors.black)),
+          iconTheme: IconThemeData(
+            color: Get.isDarkMode
+                ? Colors.white
+                : Colors.black, //change your color here
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(20),
-        itemCount: detailJuz.verses!.length,
-        itemBuilder: (context, index) {
-          if(detailJuz.verses?.length == 0 || detailJuz.verses == null) {
-            return const Center(
-              child: Text("Tidak ada data."),
-            );
-          }
-          juz.Verses ayat = detailJuz.verses![index];
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Get.isDarkMode ? Colors.black38 : Colors.grey[100] ),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(20),
+          itemCount: detailJuz.verses!.length,
+          itemBuilder: (context, index) {
+            if (detailJuz.verses?.length == 0 || detailJuz.verses == null) {
+              return const Center(
+                child: Text("Tidak ada data."),
+              );
+            }
+            juz.Verses ayat = detailJuz.verses![index];
 
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 10, vertical: 5),
-                child: Row(
-                  mainAxisAlignment:
-                  MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage(Get.isDarkMode
-                                  ? "assets/images/list_dark.png"
-                                  : "assets/images/list2.png"))),
-                      child: Center(
-                        child: Text("${ayat.number?.inSurah}"),
-                      ),
-                    ),
-                    Row(
+            if (index != 0) {
+              if (ayat.number?.inSurah == 1) {
+                controller.index++;
+              }
+            }
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color:
+                          Get.isDarkMode ? Colors.black38 : Colors.grey[100]),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                                Icons.bookmark_add_outlined)),
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.play_arrow)),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 10),
+                              height: 35,
+                              width: 35,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(Get.isDarkMode
+                                          ? "assets/images/list_dark.png"
+                                          : "assets/images/list2.png"))),
+                              child: Center(
+                                child: Text("${ayat.number?.inSurah}"),
+                              ),
+                            ),
+                            Text(
+                              allSurahInJuz[controller.index]
+                                      .name
+                                      ?.transliteration
+                                      ?.id ??
+                                  "",
+                              style: const TextStyle(
+                                  fontStyle: FontStyle.italic, fontSize: 16),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.bookmark_add_outlined)),
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.play_arrow)),
+                          ],
+                        )
                       ],
-                    )
-                  ],
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              "${ayat.text?.arab}",
-              style: const TextStyle(fontSize: 25),
-              textAlign: TextAlign.end,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              "${ayat.text?.transliteration?.en}",
-              style: const TextStyle(
-                  fontSize: 18, fontStyle: FontStyle.italic),
-              textAlign: TextAlign.end,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              "${ayat.translation?.id}",
-              style: const TextStyle(fontSize: 16),
-              textAlign: TextAlign.justify,
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-          ],
-        );
-      },)
-    );
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "${ayat.text?.arab}",
+                  style: const TextStyle(fontSize: 25),
+                  textAlign: TextAlign.end,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "${ayat.text?.transliteration?.en}",
+                  style: const TextStyle(
+                      fontSize: 18, fontStyle: FontStyle.italic),
+                  textAlign: TextAlign.end,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "${ayat.translation?.id}",
+                  style: const TextStyle(fontSize: 16),
+                  textAlign: TextAlign.justify,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
+            );
+          },
+        ));
   }
 }
