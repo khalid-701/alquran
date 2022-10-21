cdb6ed51104cb02fe616bcc371b1b98dbcdce0be
khalid-701 <khalidramdhan97@gmail.com>
2022-10-21 16:54:05 +0700

    switch

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:alquran/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 import '../../../data/models/juz/juz.dart' as juz;
 import '../../../data/models/surah/surah.dart';
@@ -22,27 +23,54 @@ class HomeView extends GetView<HomeController> {
         title: const Text("Al Quran Apps"),
         centerTitle: true,
         actions: [
+          // Obx(() {
+          //   return IconButton(
+          //       onPressed: () => Get.toNamed(Routes.SEARCH),
+          //       icon: Icon(
+          //         Icons.search,
+          //         color: controller.isDarkMode.isTrue
+          //             ? Colors.white
+          //             : Colors.black,
+          //       ));
+          // }),
           Obx(() {
-            return IconButton(
-                onPressed: () => Get.toNamed(Routes.SEARCH),
-                icon: Icon(
-                  Icons.search,
-                  color: controller.isDarkMode.isTrue
-                      ? Colors.white
-                      : Colors.black,
-                ));
-          }),
-          Obx(() {
-            return IconButton(
-                onPressed: () {
+            return Container(
+              width: 100,
+              height: 40,
+              child: LiteRollingSwitch(
+                //initial value
+                value: controller.isDarkMode.value,
+                textOn: 'Light',
+                textOff: 'Dark',
+                colorOn: Colors.green,
+                colorOff: Colors.red,
+                iconOn: Icons.light_mode,
+                iconOff: Icons.dark_mode,
+                textSize: 16.0,
+                onChanged: (bool state) {
+                  print('Current State of SWITCH IS: $state');
+                },
+                onTap: () {
                   controller.changeTheme();
                 },
-                icon: Icon(
-                  Icons.color_lens,
-                  color: controller.isDarkMode.isTrue
-                      ? Colors.white
-                      : Colors.black,
-                ));
+                onDoubleTap: () {
+                  controller.changeTheme();
+                },
+                onSwipe: () {
+                  controller.changeTheme();
+                },
+              ),
+            );
+            // return IconButton(
+            //     onPressed: () {
+            //       controller.changeTheme();
+            //     },
+            //     icon: Icon(
+            //       Icons.color_lens,
+            //       color: controller.isDarkMode.isTrue
+            //           ? Colors.white
+            //           : Colors.black,
+            //     ));
           })
         ],
       ),
@@ -268,38 +296,37 @@ class HomeView extends GetView<HomeController> {
               }),
               Expanded(
                   child: TabBarView(
-                    children: [
-                      //SURAH
-                      FutureBuilder<List<Surah>>(
-                        future: controller.getAllSurah(),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          }
-                          if (!snapshot.hasData) {
-                            return const Center(child: Text("Tidak ada data"));
-                          }
-                          return ListView.builder(
-                              itemCount: snapshot.data!.length,
-                              itemBuilder: (context, index) {
-                                Surah surah = snapshot.data![index];
-                                return ListTile(
-                                  onTap: () {
-                                    Get.toNamed(Routes.DETAIL_SURAH, arguments: {
-                                      "name": surah.name!.transliteration!.id,
-                                      "number": surah.number
-                                    });
-                                  },
-                                  leading: Obx(() {
-                                    return Container(
-                                      height: 35,
-                                      width: 35,
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                              image: AssetImage(controller
+                children: [
+                  //SURAH
+                  FutureBuilder<List<Surah>>(
+                    future: controller.getAllSurah(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (!snapshot.hasData) {
+                        return const Center(child: Text("Tidak ada data"));
+                      }
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            Surah surah = snapshot.data![index];
+                            return ListTile(
+                              onTap: () {
+                                Get.toNamed(Routes.DETAIL_SURAH, arguments: {
+                                  "name": surah.name!.transliteration!.id,
+                                  "number": surah.number
+                                });
+                              },
+                              leading: Obx(() {
+                                return Container(
+                                  height: 35,
+                                  width: 35,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(controller
                                                   .isDarkMode.isTrue
                                               ? "assets/images/list_dark.png"
                                               : "assets/images/list2.png"))),
@@ -426,11 +453,14 @@ class HomeView extends GetView<HomeController> {
                               return Obx(() {
                                 return ListTile(
                                   onTap: () {
-                                    Get.toNamed(Routes.DETAIL_SURAH, arguments: {
-                                      "name": data['surah'].toString().replaceAll("+", "'"),
-                                      "number": data['number_surah'],
-                                      "bookmark" : data,
-                                    });
+                                    Get.toNamed(Routes.DETAIL_SURAH,
+                                        arguments: {
+                                          "name": data['surah']
+                                              .toString()
+                                              .replaceAll("+", "'"),
+                                          "number": data['number_surah'],
+                                          "bookmark": data,
+                                        });
                                   },
                                   leading: Container(
                                     height: 35,
